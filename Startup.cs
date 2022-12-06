797f615f21a197f92e64d43060e069affab11db1
KNIGHT007 <83448150+janasayan0508@users.noreply.github.com>
2022-12-06 19:40:43 +0530

    update

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eLogSheet.Startup))]
+namespace eLogSheet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
